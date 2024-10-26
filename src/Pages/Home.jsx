ee2813ff0816f507311d2b499c25f5d0ca2331ec
Deepakmuthu21 <deepakmuthu21@gmail.com>
2024-10-26 17:21:22 +0530

    update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -43,7 +43,7 @@ const Home = ({email}) => {
     <div>
           <Header/>
 
-      <div className="m-5  row row-cols-1 row-cols-md-3 g-4">
+      <div className="m-5  row row-cols-1 row-cols-md-3 row-cols-sm-2 g-4">
       
       {products.map((element,index)=>{ 
 
